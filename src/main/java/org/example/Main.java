505bd73d75a669b35f5ac26c5025154891b1b65f
Aum-kar <1977omkarpatil@gmail.com>
2026-05-13 18:01:04 +0530

    Separating concerns

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,16 @@ import technology.tabula.RectangularTextContainer;
 import technology.tabula.Table;
 import technology.tabula.extractors.SpreadsheetExtractionAlgorithm;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileWriter;
 import java.util.List;
+
+import static org.example.PdfParser.getRowData;
 
 public class Main {
     public static void main(String[] args) {
-        String filepath = "C:\\Users\\omkar\\IdeaProjects\\parse-pdf\\pdfsource\\eStatement-2025_11.pdf";
+        String filepath = "C:\\Users\\omkar\\IdeaProjects\\parse-pdf\\pdfsource\\eStatement-2026_04.pdf";
         try {
             File file = new File(filepath);
             PDDocument document = Loader.loadPDF(file);
@@ -21,6 +24,11 @@ public class Main {
             SpreadsheetExtractionAlgorithm sea = new SpreadsheetExtractionAlgorithm();
             int totalPages = document.getNumberOfPages();
 
+            BufferedWriter writer =
+                    new BufferedWriter(new FileWriter("C:\\Users\\omkar\\IdeaProjects\\parse-pdf\\pdfsource\\textOutputFiles\\test.csv"));
+
+
+            System.out.println("Date\tValue Date\tDescription\tCheque\tDeposits\tWithdrawal\tBalance");
             for (int i = 1; i <= totalPages; i++) {
                 List<Table> tables = sea.extract(extractor.extract(i));
 
@@ -30,89 +38,27 @@ public class Main {
                     for (List<RectangularTextContainer> row : rows) {
                         List<String> cells = getRowData(row);
 
+
+
                         if (cells != null) {
-                            System.out.println("Date: " + cells.get(0) +
-                                    "\nValue Date: " + cells.get(1) +
-                                    "\nDescription: " + cells.get(2) +
-                                    "\nCheque: " + cells.get(3) +
-                                    "\nDeposit: " + cells.get(4) +
-                                    "\nWithdrawal: " + cells.get(5) +
-                                    "\nBalance: " + cells.get(6) + "\n"
+                            writer.write(cells.get(0) +
+                                    ", " + cells.get(1) +
+                                    ", " + cells.get(2) +
+                                    ", " + cells.get(3) +
+                                    ", " + cells.get(4) +
+                                    ", " + cells.get(5) +
+                                    ", " + cells.get(6) + "\n"
                             );
                         }
                     }
                 }
             }
             document.close();
+            writer.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static List<String> getRowData(List<RectangularTextContainer> row) {
 
-        List<String> cells = new ArrayList<>();
-
-        /*
-         * Build complete row first
-         */
-        for (RectangularTextContainer cell : row) {
-
-            String text = cell.getText()
-                    .replace("\r", "")
-                    .replace("\n", "")
-                    .trim();
-
-            // remove commas only from numeric values
-            if (text.matches("[0-9,]+\\.?[0-9]*"))
-                text = text.replace(",", "");
-
-            cells.add(text);
-        }
-
-        /*
-         * Skip repeated page headers
-         */
-        boolean isHeader = cells.contains("Value Date") || cells.contains("ValueDate")
-                        && cells.contains("Description")
-                        && cells.contains("Withdrawal");
-
-        if (isHeader)
-            return null;
-
-        /*
-         * Skip TOTAL row
-         */
-        boolean isTotal = cells
-                .stream()
-                .anyMatch(c -> c.equalsIgnoreCase("Total"));
-
-        if (isTotal)
-            return null;
-
-        /*
-         * Merge split balance column
-         */
-        if (cells.size() >= 8) {
-            String secondLast = cells.get(cells.size() - 2);
-            String last = cells.get(cells.size() - 1);
-
-            if (secondLast.matches("\\d+")
-                    && last.matches("\\d+\\.\\d+")) {
-
-                String mergedBalance = secondLast + last;
-
-                cells.remove(cells.size() - 1);
-                cells.set(cells.size() - 1, mergedBalance);
-            }
-        }
-
-        /*
-         * Ensure exactly 7 columns
-         */
-        while (cells.size() < 7)
-            cells.add("");
-
-        return cells;
-    }
 }
---
+++ src/main/java/org/example/PdfParser.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import technology.tabula.RectangularTextContainer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PdfParser {
+
+    static List<String> getRowData(List<RectangularTextContainer> row) {
+
+        List<String> cells = new ArrayList<>();
+
+        /*
+         * Build complete row first
+         */
+        for (RectangularTextContainer cell : row) {
+
+            String text = cell.getText()
+                    .replace("\r", "")
+                    .replace("\n", "")
+                    .replace(",", "")
+                    .trim();
+
+            // remove commas only from numeric values
+            if (text.matches("[0-9,]+\\.?[0-9]*"))
+                text = text.replace(",", "");
+
+            cells.add(text);
+        }
+
+        /*
+         * Skip repeated page headers
+         */
+        boolean isHeader = cells.contains("Value Date") || cells.contains("ValueDate")
+                        && cells.contains("Description")
+                        && cells.contains("Withdrawal")
+                        || cells.contains("BALANCE FORWARD");
+
+        if (isHeader)
+            return null;
+
+        /*
+         * Skip TOTAL row
+         */
+        boolean isTotal = cells
+                .stream()
+                .anyMatch(c -> c.equalsIgnoreCase("Total"));
+
+        if (isTotal)
+            return null;
+
+        /*
+         * Merge split balance column
+         */
+        if (cells.size() >= 8) {
+            String secondLast = cells.get(cells.size() - 2);
+            String last = cells.get(cells.size() - 1);
+
+            if (secondLast.matches("\\d+")
+                    && last.matches("\\d+\\.\\d+")) {
+
+                String mergedBalance = secondLast + last;
+
+                cells.remove(cells.size() - 1);
+                cells.set(cells.size() - 1, mergedBalance);
+            }
+        }
+
+        /*
+         * Ensure exactly 7 columns
+         */
+        while (cells.size() < 7)
+            cells.add("");
+
+        return cells;
+    }
+}
